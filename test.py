2ebf676e8de0b3fa8d51eae031fdb08783d40b22
153forever <44022250+153forever@users.noreply.github.com>
2021-12-10 12:24:36 +0300

    Wrong Steganographer name

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from Audio import Audio
-from Steganography import Steganography
+from Steganographer import Steganographer
 from BBS import BBS
 import numpy as np
 import rsa
@@ -9,10 +9,10 @@ from Crypto.Random import get_random_bytes
 public, private = rsa.newkeys(512)
 alg = BBS()
 audio = Audio(filename=r'source.wav')
-crypter = Steganography(audio=audio, algorithm=alg)
+crypter = Steganographer(audio=audio, algorithm=alg)
 crypter.encrypt("Hello world!"*100, filename=r'source_new.wav', publicKey=public)
 audio2 = Audio(filename=r'source_new.wav')
 session_key = rsa.decrypt(bytes(audio2.getkey(512)), private)
 session_key = session_key.decode('utf-8')
-crypter2 = Steganography(audio=audio2, key=session_key)
+crypter2 = Steganographer(audio=audio2, key=session_key)
 print(crypter2.decode())
